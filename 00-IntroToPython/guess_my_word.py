886baaf0687498a6013700ef3fb25af88b2aeb77
BananaEater2468 <pck2468@outlook.com>
2025-06-04 12:57:26 -0700

    Commit Test - Initial Test

--- 00-IntroToPython/guess_my_word.py
+++ 00-IntroToPython/guess_my_word.py
@@ -1,30 +1,49 @@
 import math
 import random
 
-# Intro
-print("Guess My Word")
-
-#Random Word
-word_options = ['burger', 'funky', 'frogs', 'block']
-secret_word = random.choice(word_options)
-print(secret_word) #TODO: Del
-
-# Get String Length
-word_length = len(secret_word)
-print(word_length) #TODO: Del
-
-# Print Display Word in *'s
-display_word = "*" * word_length
+def update_displayword(dw,sw,g):
 
 
-# Loop {
-# Prompt For a Letter
-# Check the Letter has not Already Been Guessed
-# Else - add to "guessed letters" list
+    # Letter by letter, check to see if the letter is in the word
 
-# Letter by letter, check to see if the letter is in the word
-# Update display word - print it
-# if display_word = secret_word, break
-# Loop }
+def main():
 
-# print congrats and amount of guesses
+    # Intro
+    print("Guess My Word")
+
+    #Random Word
+    word_options = ['burger', 'funky', 'frogs', 'block']
+    secret_word = random.choice(word_options)
+    print(secret_word) #TODO: Del
+
+    # Get String Length
+    word_length = len(secret_word)
+    print(word_length) #TODO: Del
+
+    # Print Display Word in *'s
+    display_word = "*" * word_length
+
+    # Empty List of Guessed Letters
+    guessed_letters = []
+
+
+    while display_word != secret_word: # loop
+        guess = input("Guess a Letter: ") # Prompt For a Letter
+        if len(guess) != 1: # Make sure guess is one letter
+            print("Too many letters :(") # Response
+            continue # we're done here
+        if guess in guessed_letters: # Make sure the guess is not a duplicate
+            print("You already guessed", guess, "!")
+            continue
+        guessed_letters.append(guess) # Add the guessed letter to the list of letters that have been guessed
+
+
+
+
+    # Update display word - print it
+    # if display_word = secret_word, break
+    # Loop }
+
+    # print congrats and amount of guesses
+
+main()
