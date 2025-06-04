7e4c823fe523dd8855f8f9ce5353f2921e78d071
BananaEater2468 <pck2468@outlook.com>
2025-06-04 12:35:03 -0700

    Commit Test - Initial Test

--- 00-IntroToPython/guess_my_word.py
+++ 00-IntroToPython/guess_my_word.py
@@ -8,3 +8,15 @@ print("Guess My Word")
 secret_word = "dog"
 
 # Get String Length & Display Word in *'s
+
+# Loop {
+# Prompt For a Letter
+# Check the Letter has not Already Been Guessed
+# Else - add to "guessed letters" list
+
+# Letter by letter, check to see if the letter is in the word
+# Update display word - print it
+# if display_word = secret_word, break
+# Loop }
+
+# print congrats and amount of guesses
