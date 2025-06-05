2e81af40247fe7315bed3141340412887227293b
BananaEater2468 <pck2468@outlook.com>
2025-06-05 08:34:57 -0700

    created text x2

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -25,6 +25,8 @@ def main():
 
     # Prepare the text caption(s)
     # TODO 4: Create a font object with a size 28 font.
+    font1 = pygame.font.SysFont("corbel", 28)
+    caption1 = font1.render("Two Dogs", True, BLACK)
     # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
 
     # Prepare the music
@@ -47,8 +49,12 @@ def main():
         # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
-
+        screen.blit(caption1, (screen.get_width() / 2 - caption1.get_width() / 2, image.get_height() - 2))
         # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        font2 = pygame.font.SysFont("ariel", 50)
+        caption2 = font2.render("DIBS!", True, BLACK)
+        screen.blit(caption2, (screen.get_width() / 2 - caption2.get_width() / 2, image.get_height() - 100))
+
 
         # Update the screen
         pygame.display.update()
